0d4f34d244cf3d7ba7cdc0d768c6ec941415ce0a
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-11-29 15:15:01 +0100

    Test méthode

--- Mini_Projet/src/mini_projet/Mini_Projet.java
+++ Mini_Projet/src/mini_projet/Mini_Projet.java
@@ -14,17 +14,41 @@ public class Mini_Projet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    CelluleLumineuse cellule1 = new CelluleLumineuse();
+        CelluleLumineuse cellule2 = new CelluleLumineuse();
 
-        CelluleLumineuse cellule = new CelluleLumineuse();
-        System.out.println(cellule); // Devrait afficher "O" (éteinte)
-        
-        cellule.activerCellule();
-        System.out.println(cellule); // Devrait afficher "X" (allumée)
-        
-        cellule.eteindreCellule();
-        System.out.println(cellule); // Devrait afficher "O" (éteinte)
-        
-        System.out.println("État actuel : " + (cellule.getEtat() ? "Allumée" : "Éteinte"));
-        System.out.println("Est éteinte ? " + cellule.estEteint());
+        // Test initial des états des cellules
+        System.out.println("État initial des cellules :");
+        System.out.println("Cellule 1 : " + cellule1); // Devrait afficher "O" (éteinte)
+        System.out.println("Cellule 2 : " + cellule2); // Devrait afficher "O" (éteinte)
+
+        // Activation des cellules
+        cellule1.activerCellule();
+        cellule2.activerCellule();
+
+        System.out.println("\nAprès activation des cellules :");
+        System.out.println("Cellule 1 : " + cellule1); // Devrait afficher "X" (allumée)
+        System.out.println("Cellule 2 : " + cellule2); // Devrait afficher "X" (allumée)
+
+        // Extinction de la cellule 1, cellule 2 reste inchangée
+        cellule1.eteindreCellule();
+
+        System.out.println("\nAprès extinction de la cellule 1 :");
+        System.out.println("Cellule 1 : " + cellule1); // Devrait afficher "O" (éteinte)
+        System.out.println("Cellule 2 : " + cellule2); // Devrait afficher "X" (allumée)
+
+        // Vérification des méthodes estEteint() et getEtat()
+        System.out.println("\nVérification des états :");
+        System.out.println("Cellule 1 est éteinte ? " + cellule1.estEteint()); // Devrait être true
+        System.out.println("Cellule 2 est éteinte ? " + cellule2.estEteint()); // Devrait être false
+
+        System.out.println("Cellule 1 état brut : " + cellule1.getEtat()); // Devrait être false
+        System.out.println("Cellule 2 état brut : " + cellule2.getEtat()); // Devrait être true
+
+        // Activation de la cellule 1 à nouveau
+        cellule1.activerCellule();
+
+        System.out.println("\nAprès réactivation de la cellule 1 :");
+        System.out.println("Cellule 1 : " + cellule1); // Devrait afficher "X" (allumée)
     }
 }
