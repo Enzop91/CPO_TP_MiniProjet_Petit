588acff2cbb9a55cdd54818e2aacbe3b4f8a5574
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-12-11 15:17:58 +0100

    2e diagonale

--- LightOff_PETIT_version_graphique/src/CelluleGraphique.java
+++ LightOff_PETIT_version_graphique/src/CelluleGraphique.java
@@ -42,9 +42,9 @@ public class CelluleGraphique extends JButton {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (celluleLumineuseAssociee.getEtat()) {
-            setBackground(Color.YELLOW); // Couleur quand la lumière est allumée
+            setBackground(Color.PINK); // Couleur quand la lumière est allumée
         } else {
-            setBackground(Color.BLACK); // Couleur quand la lumière est éteinte
+            setBackground(Color.WHITE); // Couleur quand la lumière est éteinte
         }
     }
 }
